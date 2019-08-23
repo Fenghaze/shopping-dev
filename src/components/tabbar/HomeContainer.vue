a801af3b5e9c1510f68b36dfa736ccb6c028f9fa
zhl <feng_haze@163com>
2019-08-23 15:39:47 +0800

    HomeContainer add swipe

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <h1>HomeContainer</h1>
+        <mt-swipe :auto="4000">
+            <mt-swipe-item>1</mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
     </div>
 </template>
 
@@ -9,10 +13,25 @@ export default {
     data() {
         return {};
     },
-    methods: {},
+    methods: {
+    },
 };
 </script>
 
 <style scoped lang="scss">
+.mint-swipe {
+    height: 200px;
 
+    .mint-swipe-item {
+        &:nth-child(1){
+            background-color: red;
+        }
+        &:nth-child(2){
+            background-color: blue;
+        }
+        &:nth-child(3){
+            background-color: yellow;
+        }
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -8,9 +8,13 @@ import app from './App.vue'
 
 import VueRouter from 'vue-router'
 import router from './router.js'
+// 数据请求，需要用到 vue-resource
+import VueResource from 'vue-resource'
+
 
 Vue.use(MintUI)
 Vue.use(VueRouter)
+Vue.user(VueResource)
 
 
 var vm = new Vue({
