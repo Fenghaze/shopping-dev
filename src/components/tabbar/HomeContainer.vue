2207f530ca9860b9febe686021d9d7a109ab9eb7
zhl <feng_haze@163com>
2019-08-23 15:22:12 +0800

    add router function

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>HomeContainer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,21 @@
 import VueRouter from 'vue-router'
 
+// 导入对应的路由组件
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import VipContainer from './components/tabbar/VipContainer.vue'
+import CartContainer from './components/tabbar/CartContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+
+
 var router = new VueRouter({
     routes:[
-        
-    ]
+        {path: '/home', component: HomeContainer},
+        {path: '/vip', component: VipContainer},
+        {path: '/cart', component: CartContainer},
+        {path: '/search', component: SearchContainer}
+    ],
+    // 覆盖默认的路由高亮的类
+    linkActiveClass: 'mui-active'
 })
 
 export default router
