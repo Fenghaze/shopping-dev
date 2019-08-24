b31cf2f05e9110c94449939e803dcf624fc00ea1
zhl <feng_haze@163com>
2019-08-24 16:00:48 +0800

    add grid to HomeConntainer

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -5,6 +5,27 @@
                 <img :src="item.url" alt="">
             </mt-swipe-item>
         </mt-swipe>
+        <!-- 六宫格 -->
+        <ul class="mui-table-view mui-grid-view mui-grid-9">
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-home"></span>
+		                    <div class="mui-media-body">新闻资讯</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-email"></span>
+		                    <div class="mui-media-body">图片分享</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-chatbubble"><span class="mui-badge">9</span></span>
+		                    <div class="mui-media-body">商品购买</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-location"></span>
+		                    <div class="mui-media-body">留言反馈</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-search"></span>
+		                    <div class="mui-media-body">视频专区</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-phone"></span>
+		                    <div class="mui-media-body">联系我们</div></a></li>
+		        </ul> 
     </div>
 </template>
 
